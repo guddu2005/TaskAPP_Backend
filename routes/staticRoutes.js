c2a1b87955590483e4f669257493f291d8ac3355
Guddu Kumar <gkazad112006@gmail.com>
2024-08-18 15:03:07 +0530

    taskApp

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const staticRouter = express.Router();
 
+staticRouter.get("/" ,(req,res) =>{
+    return res.render('start');
+})
 staticRouter.get("/signup", (req, res) => {
     return res.render("signup");
 });
